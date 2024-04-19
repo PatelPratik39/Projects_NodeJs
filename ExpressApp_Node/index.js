618fadb4f5d9c6960b46d04b67f28502f44cca89
PatelPratik39 <ptl.pratik717@gmail.com>
2024-04-19 13:33:22 -0500

    upload image usinf built in middleware

--- ExpressApp_Node/index.js
+++ ExpressApp_Node/index.js
@@ -1,42 +1,98 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const multer = require("multer");
+const upload = multer({ dest: "./public/upload" });
 const app = express();
+const router = express.Router();
 const PORT = 5001;
 
-app.get('/', (req, res) =>{
-    res.json({
-        message : "This is our HOME Page!!1"
-    })
+// inbuild middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// express.static()
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+// middleware
+const loggerMiddleWare = (req, res, next) => {
+  console.log(`${new Date()} --- REAUEST [ ${req.method}] [${req.url}]`);
+  next();
+};
+
+app.use(logger("combined"));
+
+app.use(loggerMiddleWare);
+app.use("/api/users", router);
+
+// fake auth Middleware Function
+const fakeAuth = (req, res, next) => {
+  const authStatus = true;
+  if (authStatus) {
+    console.log("Use Auth Status : ", authStatus);
+    next();
+  } else {
+    res.status(401);
+    throw new Error("User is not Authorized");
+  }
+};
+const getUsers = (req, res) => {
+  console.log("This is request Body from Client : ", req.body);
+  res.json({ message: "Gell All Users" });
+};
+const createUser = (req, res) => {
+  res.json({ message: "Create New Users" });
+};
+
+router.use(fakeAuth);
+router.route("/").get(getUsers).post(createUser);
+
+// ErrorHandlerMiddleware
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  switch (statusCode) {
+    case 401:
+      res.json({
+        title: "Unathorized",
+        message: error.message
+      });
+      break;
+    case 404:
+      res.json({
+        title: "Not Found",
+        message: error.message
+      });
+      break;
+    case 500:
+      res.json({
+        title: "Server Error",
+        message: error.message
+      });
+
+    default:
+      break;
+  }
+};
+// upload image through middleware and postman
+app.post(
+  "/upload",
+  upload.single("image"),
+  (req, res, next) => {
+    console.log(req.file, req.body);
+    res.send(req.file);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  throw new Error("Route Not FOund!!");
 });
 
-app.get("/users", (req, res) => {
-  res.json({
-    message: "Get All Users"
-  });
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT : ${PORT}`);
 });
-
-app.get("/users/:id", (req, res) => {
-  res.json({
-    message: `GET User with ID ${req.params.id}`
-  });
-});
-
-app.post("/users/:id", (req, res) => {
-  res.json({
-    message: `POST a new User`
-  });
-});
-app.put("/users/:id", (req, res) => {
-  res.json({
-    message: `UPDATE User with ID ${req.params.id}`
-  });
-});
-
-app.delete("/users/:id", (req, res) => {
-  res.json({
-    message: `DELETE User with ID ${req.params.id}`
-  });
-});
-
-app.listen(PORT , ()=>{
-    console.log(`Your App is Running on PORT : ${PORT}`);
-})
